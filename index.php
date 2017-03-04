25be828117fcc99c89be808020478692244e22c9
Belynda Thys <bely.thys@gmail.com>
2017-03-04 18:28:58 +0100

    config - test

--- index.php
+++ index.php
@@ -16,17 +16,7 @@
 <body>
 
 <?php
-
-$host="localhost";
-$dbName="leo";
-$user="root";
-$password="root";
-
-try{
-    $db = new PDO('mysql:host='.$host.';dbname='.$dbName.';charset=utf8',$user,$password);
-} catch (Exception $e){
-    die('Erreur :'.$e->getMessage());
-}
+include ('config/config.php');
 
 if(!empty($_POST['name']) && (!empty($_POST['firstname'])) && (!empty($_POST['email'])) ){
 
